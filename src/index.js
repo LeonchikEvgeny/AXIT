816ed23522dfe30e4bff7070d9ce2326ea18fa3d
evgeny <mr.leon.evg@gmail.com>
2022-05-05 18:30:28 +0300

    add all

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './css/about/about.scss';
+import './css/banner/banner.scss';
+import './css/body/body.scss';
+import './css/contact/contact.scss';
+import './css/customers/customers.scss';
+import './css/footer/footer.scss';
+import './css/pricing/pricing.scss';
+import './css/social/social.scss';
+import './css/stylish/stylish.scss';
+import './css/tabs/tabs.scss';
+import './css/reset.scss';
+import './sass/style.scss';
